38bb6d47bce8e800f09a22a274653c54591fc36f
unknown <vojtpavlo3@gmail.com>
2020-06-04 15:31:23 +0300

    Q

---
+++ HW10/index.js
@@ -0,0 +1,10 @@
+alert("Hello, world!!");
+// console.log("Hello world");
+
+let a = "hello world";
+let u= "what are you doing";
+let e = "freely done?";
+
+console.log(a)
+console.log(u)
+console.log(e)
